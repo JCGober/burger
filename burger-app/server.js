bc6c20b1d5626a16063bc41c90c5c94ab23ca09f
Jared C. Gober <jcgober@Jareds-MacBook-Air.local>
2020-09-10 13:58:53 -0400

    Formatted server.js

---
+++ burger-app/server.js
@@ -0,0 +1,30 @@
+
+// Set up for Express
+var express = require("express");
+
+
+// Port with HEROKU deploy
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Set up Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Imported Routes
+var routes = require("./controllers/burgersController.js");
+
+app.use(routes);
+
+// Server Start up
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
