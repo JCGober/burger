7312ffd6d9466d9779ce72d2eb3b66896f2e5405
Jared C. Gober <jcgober@Jareds-MacBook-Air.local>
2020-09-10 21:16:30 -0400

    All parts working

--- burger-app/public/assets/js/burgers.js
+++ burger-app/public/assets/js/burgers.js
@@ -1,20 +1,20 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".change-sleep").on("click", function(event) {
+    $(".change-eat").on("click", function(event) {
       var id = $(this).data("id");
-      var newSleep = $(this).data("newsleep");
+      var newEat = $(this).data("neweat");
   
-      var newSleepState = {
+      var newEatState = {
         eaten: newEat
       };
   
       // Send the PUT request.
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
-        data: newEat
+        data: newEatState
       }).then(
         function() {
-          console.log("changed sleep to", newEat);
+          console.log("changed eat", newEat);
           // Reload the page to get the updated list
           location.reload();
         }
